0f0cc49ee28b4ab4912857f17ab30978494a0bbe
Cory Evans <cory.evans@mailbox.org>
2021-11-04 16:00:03 +1300

    created faq page

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,22 @@ import Header from "./Header";
 import { Switch, Route } from "react-router-dom";
 import JobSheet from "./JobSheet";
 import Advanced from "./Advanced";
+import FAQ from "./FAQ";
 
 class App extends React.Component {
 	render() {
 		return (<>
 		<Switch>
-			<Route path="/jobsheet">
-				<JobSheet />
-			</Route>
 			<Route path="/advanced">
 				<Header title="Javascript Extension" />
 				<Advanced />
+			</Route>
+			<Route path="/faq">
+				<Header title="Phone Fix Booking FAQs" />
+				<FAQ />
+			</Route>
+			<Route path="/jobsheet">
+				<JobSheet />
 			</Route>
 			<Route path="/">
 				<Header title="Phone Booking System" />
---
+++ src/components/FAQ.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class FAQ extends React.Component {
+	render() {
+		return (
+			<div>
+			</div>
+		)
+	}
+}
+
+export default FAQ;
