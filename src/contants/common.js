c7a9773fbf7b2cf1e311a60e91368fe645ece6fd
Cory Evans <cory.evans@mailbox.org>
2021-11-15 16:00:08 +1300

    configureable url prefix

--- src/contants/common.js
+++ src/contants/common.js
@@ -1,1 +1,10 @@
-export const URL_PREFIX = process.env.PUBLIC_URL ? process.env.PUBLIC_URL : '';
+// TODO: Make the number of path segments configurable
+
+export const URL_PREFIX = process.env.PUBLIC_URL ? 
+	(() => {
+		const u = new URL(process.env.PUBLIC_URL)
+		const segments  = u.pathname.split('/')
+		if (segments.length > 1) return '/' + segments('/')[1]
+		return ''
+	})()
+: '';
